962ccf7ab94abd7560dda08b336bfedaf946c55d
Coolguyifez <104318594+Coolguyifez@users.noreply.github.com>
2026-03-30 20:57:29 +0100

    Add files via upload

---
+++ static/sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'coreefx-cache-v1';
+const urlsToCache = [
+  '/',
+  '/static/images/brain.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
