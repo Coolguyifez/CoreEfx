32b1bd75316d2034f99036248231c5fef0c0015b
Coolguyifez <104318594+Coolguyifez@users.noreply.github.com>
2026-04-17 06:32:49 +0100

    Update CoreEfx.py

--- CoreEfx.py
+++ CoreEfx.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template, jsonify, url_for, flash
+from flask import Flask, request, redirect, render_template, jsonify, url_for, flash, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, timedelta
 import spacy
